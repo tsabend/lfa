6d653309ab7a790b1d62843c4e2ce12650e33c75
tsabend <tsabend@gmail.com>
2015-02-07 10:52:53 -0500

    Remove site folder

--- site/components/book.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-
-// var renderPage = function() {
-  React.render(
-    <AllBooks />,
-    document.getElementById('books')
-  )
-// }
--- site/library.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  var searchBar = document.getElementById('searchBar')
-  console.log(searchBar)
-  window.addEventListener('scroll', function() {
-    searchBar.getBoundingClientRect().top < 0 ?
-    searchBar.classList.add('stuck') :
-    searchBar.classList.remove('stuck');
-});
-}
