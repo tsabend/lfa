a934f56386787628b8e906a3a5e5c70543a03328
tsabend <tsabend@gmail.com>
2015-02-09 11:22:03 -0500

    Remove commented code from node server

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 // Server
 var express = require('express')
 app = express()
-// Require middleware for parsing json
-// var bodyParser = require('body-parser')
-// CORS
-// var cors = require('cors')
-
-// app.use(cors())
-// app.use(bodyParser())
 
 var path = require('path');
 
-app.use(require('express-jsxtransform')())
-  .use(express.static(path.join(__dirname, 'public')));
+app..use(express.static(path.join(__dirname, 'public')));
 app.engine('html', require('ejs').renderFile);
-
-
 
 app.route('/').get(function(req, res){
   res.render('library.html')
